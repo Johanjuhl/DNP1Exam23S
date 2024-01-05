7b5b155a272df5b8abbae47fa5591613b85d9f8a
Johan <295029@via.dk>
2024-01-05 16:05:06 +0100

    upload

---
+++ ConsoleApp/Person.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp;
+
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class Person
+{
+    public int PersonId { get; set; }
+   
+    public string Name { get; set; }
+    
+    public char Gender { get; set; }
+    // Add other properties as needed
+}
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=bums.db");
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new MyDbContext())
+        {
+            // Ensure database is created
+            context.Database.EnsureCreated();
+
+            // Add a new person
+            var person = new Person { Name = "John Doe", Gender = 'M' };
+            context.People.Add(person);
+            context.SaveChanges();
+
+          
+            // Retrieve and display all people
+            var people = context.People.ToList();
+            foreach (var p in people)
+            {
+                Console.WriteLine($"Person ID: {p.PersonId}, Name: {p.Name}, Gender: {p.Gender}");
+            }
+        }
+    }
+}
